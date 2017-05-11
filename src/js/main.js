124d963dfb9cd32f3b870edc0dcf01f449114a5b
Miras Les <Miras Les>
2017-05-11 10:43:35 +0600

    Index page

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,28 @@ $(function(){
         $('.contacts-panel').toggleClass('open');
     });
 
+    // main menu toggle
+    // ============================================
+    $('.nav .nav-toggle').click(function(){
+        $(this).toggleClass('active');
+        $('.nav').toggleClass('nav-open');
+        $('.nav .nav-list').slideToggle();
+    });
+
+    // Main Banner sliders
+    // ============================================
+    $('.banner-sliders_item .slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        dots: true,
+        navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+        items: 1,
+        onInitialized: function(){
+            $('.slider').addClass('owl-carousel');
+            $('.banner-sliders_item .slider_content').css({'height': 'auto', 'visibility':'visible'});
+        },
+    });
 
     // Fancybox
     // ============================================
@@ -21,30 +43,67 @@ $(function(){
 
     // Owl Carosel
     // ============================================
-    $('.owl-carousel').owlCarousel({
+    $('.clients-slider').owlCarousel({
         loop: true,
-        margin: 30,
+        margin: 35,
         nav: true,
         dots: false,
-        navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+        navText: ['',''],
         responsive: {
             0:{
                 items: 2,
                 dots: true,
                 nav: false,
-                margin: 10,
+                margin: 20,
+            },
+            481: {
+                items: 3,
+                dots: true,
+                nav: false,
+                margin: 15,
             },
             768:{
-                items: 3,
+                items: 4,
                 dots: false,
                 nav: true,
             },
             992:{
-                items: 4,
+                items: 5,
+            },
+            1200: {
+                items: 7,
             }
+        },
+        onInitialized: function(){
+            $('.clients-slider').addClass('owl-carousel').css({'height': 'auto', 'visibility':'visible'});
         }
     });
 
+    // Owl Carosel
+    // ============================================
+    // $('.owl-carousel').owlCarousel({
+    //     loop: true,
+    //     margin: 30,
+    //     nav: true,
+    //     dots: false,
+    //     navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+    //     responsive: {
+    //         0:{
+    //             items: 2,
+    //             dots: true,
+    //             nav: false,
+    //             margin: 10,
+    //         },
+    //         768:{
+    //             items: 3,
+    //             dots: false,
+    //             nav: true,
+    //         },
+    //         992:{
+    //             items: 4,
+    //         }
+    //     }
+    // });
 
 });
 
