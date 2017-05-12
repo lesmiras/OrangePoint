a7c19454c17d619cac709ee359c3975b3b91ae6b
Miras Les <Miras Les>
2017-05-12 10:40:19 +0600

    News pages.Services, About, Contacts,Reviews, Vacancies

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,20 @@
 $(function(){
     $(".page").removeClass("preload");
 
+    // toggle contacts panel in header
+    // ============================================
     $('#toggle-contacts-panel').click(function(){
         $(this).toggleClass('panel-open');
         $('.contacts-panel').toggleClass('open');
     });
 
+    // equal height calc
+    // ============================================
+    if( !isBreakpoint('xs')){
+        $('.advantage_item').equalHeights();
+        $('.info-block_content').equalHeights();
+    }
+    
     // main menu toggle
     // ============================================
     $('.nav .nav-toggle').click(function(){
@@ -13,6 +22,23 @@ $(function(){
         $('.nav').toggleClass('nav-open');
         $('.nav .nav-list').slideToggle();
     });
+
+    // copy human icons in TEAM ITEM
+    // ============================================
+    $.when(
+        $('.team_item-members').each(function(){
+            var count = parseInt($(this).parentsUntil('.team_item-count').text());
+            for (var i = 0; i < count; i++) {
+                $(this).append($('<span class="member"></span>'));
+            }
+        })
+    ).then(
+        function(){
+            if( !isBreakpoint('xs')){
+                $('.team_item .team_item-members').equalHeights();
+            }
+        }
+    );
 
     // Main Banner sliders
     // ============================================
@@ -31,17 +57,9 @@ $(function(){
 
     // Fancybox
     // ============================================
-    $("[data-fancybox]").fancybox({
-        padding: 0,
-        margin: 0,
-        helpers : {
-            overlay: {
-                locked: false
-            }
-        }
-    });
+    $("[data-fancybox]").fancybox();
 
-    // Owl Carosel
+    // Clients Carosel
     // ============================================
     $('.clients-slider').owlCarousel({
         loop: true,
@@ -79,6 +97,45 @@ $(function(){
         }
     });
 
+    // Sertificates Carosel
+    // ============================================
+    $('.sertificates-slider').owlCarousel({
+        loop: true,
+        margin: 50,
+        nav: true,
+        dots: false,
+        navText: ['',''],
+        responsive: {
+            0:{
+                items: 2,
+                dots: true,
+                nav: false,
+                margin: 20,
+            },
+            481: {
+                items: 2,
+                dots: true,
+                nav: false,
+                margin: 15,
+            },
+            768:{
+                items: 3,
+                dots: false,
+                nav: true,
+            },
+            992:{
+                items: 4,
+                margin: 35,
+            },
+            1200: {
+                items: 5,
+            }
+        },
+        onInitialized: function(){
+            $('.sertificates-slider').addClass('owl-carousel').css({'height': 'auto', 'visibility':'visible'});
+        }
+    });
+
     // Owl Carosel
     // ============================================
     // $('.owl-carousel').owlCarousel({
@@ -111,3 +168,37 @@ $(function(){
 function isBreakpoint( alias ) {
     return $('.device-' + alias).is(':visible');
 }
+
+/*!
+ * Simple jQuery Equal Heights
+ *
+ * Copyright (c) 2013 Matt Banks
+ * Dual licensed under the MIT and GPL licenses.
+ * Uses the same license as jQuery, see:
+ * http://docs.jquery.com/License
+ *
+ * @version 1.5.1
+ */
+(function($) {
+
+    $.fn.equalHeights = function() {
+        var maxHeight = 0,
+            $this = $(this);
+
+        $this.each( function() {
+            var height = $(this).innerHeight();
+
+            if ( height > maxHeight ) { maxHeight = height; }
+        });
+
+        return $this.css('height', maxHeight);
+    };
+
+    // auto-initialize plugin
+    $('[data-equal]').each(function(){
+        var $this = $(this),
+            target = $this.data('equal');
+        $this.find(target).equalHeights();
+    });
+
+})(jQuery);
